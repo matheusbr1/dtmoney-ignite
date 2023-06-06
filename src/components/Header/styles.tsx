839772d3fb1e82792724b53c023494eb118e914f
Matheus Baron <matheusbaron10@gmail.com>
2023-06-05 22:57:49 -0300

    feat: create Summary component

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -6,7 +6,7 @@ export const HeaderContainer = styled.header`
 `
 
 export const HeaderContent = styled.div`
-  widthh: 100%;
+  width: 100%;
   max-width: 1120px;
   margin: 0 auto;
   padding: 0 1.5rem;
