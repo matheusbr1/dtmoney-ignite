47a61022bae88f1d0025c5024e5445c78ad63b89
Matheus Baron <matheusbaron10@gmail.com>
2023-06-08 17:15:24 -0300

    feat: create NewTransactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg'
 
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionsModal } from "../NewTransactionsModal";
+
 export function Header () {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} />
 
-        <NewTransactionButton>
-          Nova Transação
-        </NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild >
+            <NewTransactionButton>
+              Nova Transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+          
+          <NewTransactionsModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )  
