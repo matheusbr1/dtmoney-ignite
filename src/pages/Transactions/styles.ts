54305a10b6f2197ce15bb0eeb4cef01e9b7712aa
Matheus Baron <matheusbaron10@gmail.com>
2023-06-08 17:22:20 -0300

    fix: change style props in the TransactionsTable PriceHighlight

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -33,7 +33,7 @@ interface PriceHighlightProps {
 }
 
 export const PriceHighlight = styled.span<PriceHighlightProps>`
-  color: ${props => props.color === 'income' 
+  color: ${props => props.variant === 'income' 
     ? props.theme["green-300"] 
     : props.theme["red-300"]
   }
