0907adc04b37c617ae8bb13a2ab8e4dd5b65d489
Matheus Baron <matheusbaron10@gmail.com>
2023-06-13 12:34:02 -0300

    feat: add controlled type field to form into newTransactionsModal component

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
@@ -9,7 +9,7 @@ const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  // type: z.enum(['income', 'outcome'])s
+  type: z.enum(['income', 'outcome'])
 })
 
 type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
@@ -18,6 +18,7 @@ export function NewTransactionsModal () {
   const {
     handleSubmit,
     register,
+    control,
     formState: { isSubmitting }
   } = useForm<newTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema)
@@ -60,17 +61,25 @@ export function NewTransactionsModal () {
             {...register('category')}
           />
 
-          <TransactionType>
-            <TransactionTypeButton variant='income' value='income' >
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            
-            <TransactionTypeButton variant='outcome' value='outcome' >
-              <ArrowCircleDown size={24} />
-              Saídas
-            </TransactionTypeButton>
-          </TransactionType>
+          <Controller 
+            control={control}
+            name='type'
+            render={({ field }) => {
+              return (
+              <TransactionType onValueChange={field.onChange} value={field.value}>
+                <TransactionTypeButton variant='income' value='income' >
+                  <ArrowCircleUp size={24} />
+                  Entrada
+                </TransactionTypeButton>
+                
+                <TransactionTypeButton variant='outcome' value='outcome' >
+                  <ArrowCircleDown size={24} />
+                  Saídas
+                </TransactionTypeButton>
+              </TransactionType>
+              )
+            }}
+          />
 
           <button type="submit" disabled={isSubmitting} >Cadastrar</button>
         </form>
